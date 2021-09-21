73fd46b04043b2bf380625fef0a3c382f82f170b
Solayman <solayman.b@live.fr>
2021-09-21 18:29:15 +0200

    remplacement de l'algorithme naif par un algorithme dynamique

--- P7_02_optimized.py
+++ P7_02_optimized.py
@@ -1,83 +1,48 @@
 import csv
-import itertools
 
-# ouvrir le fichier algoinvest.txt et récupérer les données
+# the best ROI strategy
+def find_best_investment(remainig_money, data):
+	# fill with 0s when there's no invest
+	matrix = [[0 for i in range(remainig_money + 1)] for i in range(len(data) + 1)]
+
+	# scrolling down the number of the share
+	for x in range(1, len(data) + 1):
+		# scrolling down the invested money
+		for y in range(1, remainig_money + 1):
+			# if we can invest in this share (the share price <= remaining money)
+			if data[x-1][1] <= y:
+				# we get the max of the 2 invests (the best one)
+				matrix[x][y] = max(data[x-1][2] + matrix[x-1][y-data[x-1][1]], matrix[x-1][y])
+			else:
+				# otherwise we keep the previous one
+				matrix[x][y] = matrix[x-1][y]
+
+	# identify elements based on the sum
+	n = len(data)
+	data_selection = []
+
+	while remainig_money >= 0 and n >= 0:
+		# we get the last element of the list
+		e = data[n-1]
+		if matrix[n][remainig_money] == matrix[n-1][remainig_money-e[1]] + e[2]:
+			data_selection.append(e)
+			remainig_money -= e[1]
+		n -= 1
+
+	return matrix[-1][-1], data_selection
+
+
+# open the file algoinvest.txt and get the data
 with open('algoinvest.txt', newline='') as f:
 	read = csv.reader(f)
-	liste_action = []
-	liste_invessment = []
-	liste_benefit = []
-	max_expense = 500
+	remainig_money = 500
+	data = []
 
-	# retirer les entetes et nettoyer les données
+	# delete the headers and clean the data
 	for line in read:
-		if line[1].lstrip("\t").isdigit() and 0 < int(line[1].lstrip("\t")) <= max_expense:
-			liste_action.append(line[0])
-			liste_invessment.append(int(line[1].lstrip("\t")))
-			liste_benefit.append(int(line[2].rstrip("%").lstrip("\t")))
-	duplicated_action = []
-	duplicated_invessment = []
-	duplicated_benefit = []
-	for i in liste_benefit:
-		x = liste_benefit.count(i)
-		if x > 1:
-			index = liste_benefit.index(i)
-			duplicated_action.append(liste_action.pop(index))
-			duplicated_invessment.append(liste_invessment.pop(index))
-			duplicated_benefit.append(liste_benefit.pop(index))
+		x = line[1].lstrip("\t")
+		y = line[2].rstrip("%").lstrip("\t")
+		if x.isdigit() and 0 < int(x) <= remainig_money:
+			data.append((line[0], int(x), (int(x) * int(y) / 100)))	# name of the share, invest amount €, achievable benefit €
 
-	# trier la liste des % de benef par ordre décroissant
-	sorted_liste_benefit = sorted(liste_benefit, reverse = True)
-	expense = 0
-	liste_bought_actions = []
-	liste_total_benef = []
-
-	def optimise_it(expense):
-		for i in sorted_liste_benefit:
-			index = liste_benefit.index(i)
-			if max_expense - expense >= liste_invessment[index]:
-				del sorted_liste_benefit[sorted_liste_benefit.index(i)]
-				expense = update_it(expense, index)
-				if min(sorted_liste_benefit) <= max_expense - expense:
-					optimise_it(expense)
-				return expense
-
-	def clear_it():
-		del duplicated_action[duplicated_index], duplicated_invessment[duplicated_index], duplicated_benefit[duplicated_index], sorted_liste_benefit[0]
-
-	def update_it(expense, i, d = 0):
-		if d == 1:
-			expense += duplicated_invessment[i]
-			liste_bought_actions.append(duplicated_action[i])
-			liste_total_benef.append(duplicated_benefit[i])	#expense * duplicated_benefit[i] / 100
-		else:
-			expense += liste_invessment[i]
-			liste_bought_actions.append(liste_action[i])
-			liste_total_benef.append(liste_benefit[i])	#expense * liste_benefit[i] / 100
-		return expense
-
-	while expense < max_expense:
-		index = liste_benefit.index(sorted_liste_benefit[0])
-		if sorted_liste_benefit[0] in duplicated_benefit:
-			duplicated_index = duplicated_benefit.index(sorted_liste_benefit[0])
-			if (expense + duplicated_invessment[duplicated_index] + liste_invessment[index]) <= max_expense:
-				expense = update_it(expense, index)
-				expense = update_it(expense, duplicated_index, 1)
-				clear_it()
-			elif liste_invessment[index] >= duplicated_invessment[duplicated_index] and expense + liste_invessment[index]<= max_expense:
-				expense = update_it(expense, index)
-				clear_it()
-			elif duplicated_invessment[duplicated_index] >= liste_invessment[index] and expense + duplicated_invessment[duplicated_index]<= max_expense:
-				expense = update_it(expense, duplicated_index, 1)
-				clear_it()
-			else:
-				expense = optimise_it(expense)
-				clear_it()
-				continue
-		elif expense + liste_invessment[index] <= max_expense:
-			expense = update_it(expense, index)
-			del sorted_liste_benefit[0]
-		else:
-			expense = optimise_it(expense)
-			break
-print(expense, liste_bought_actions, liste_total_benef, sorted_liste_benefit)
+print(find_best_investment(remainig_money, data))
